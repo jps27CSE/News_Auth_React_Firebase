d6f928e70f4157ef2eefe08366bb4a6d3862296a
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-10-04 01:40:23 +0600

    firebase

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../components/Home/Home";
+import News from "../components/News/News";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,11 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader: () => fetch("/news.json"),
+      },
+      {
+        path: "/news/:id",
+        element: <News />,
       },
     ],
   },
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../../shared/Header/Header";
+import LeftSideNav from "../../shared/LeftNavbar/LeftNavbar";
+import Navbar from "../../shared/Navbar/Navbar";
+import RightSideNav from "../../shared/RightNavbar/RightNavbar";
 import BreakingNews from "./BreakingNews";
+import NewsCard from "./NewsCard";
 
 const Home = () => {
+  const news = useLoaderData();
   return (
     <div>
       <Header />
       <BreakingNews />
+      <Navbar />
+      <div className="grid grid-cols-1  md:grid-cols-4 gap-6">
+        <div>
+          <LeftSideNav />
+        </div>
+        <div className="md:col-span-2">
+          {news.map((aNews) => (
+            <NewsCard key={aNews._id} news={aNews}></NewsCard>
+          ))}
+        </div>
+        <div>
+          <RightSideNav />
+        </div>
+      </div>
     </div>
   );
 };
