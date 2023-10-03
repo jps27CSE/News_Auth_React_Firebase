800fa7cd4dfc3ad668c8620113a2448fe5bd0787
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-10-03 11:29:41 +0600

    breaking news react marque added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../shared/Header/Header";
+import BreakingNews from "./BreakingNews";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <BreakingNews />
     </div>
   );
 };
